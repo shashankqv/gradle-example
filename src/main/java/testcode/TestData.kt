c1ebf9aa0332774db26f5bd9d6f0b1151a831d21
Shashank Vivek <shashank.v@mobikwik.com>
2017-07-20 19:04:47 +0530

    Adding Kotlin Support.

---
+++ src/main/java/testcode/TestData.kt
@@ -0,0 +1,14 @@
+package testcode
+
+object TestData {
+    @JvmStatic fun main(args: Array<String>) {
+        println("hey testing")
+        println("hey testing")
+        println("hey testing")
+        println("hey testing")
+        println("hey testing")
+        println("hey testing")
+        println("hey testing")
+        println("hey testing")
+    }
+}
---
+++ src/main/kotlin/testcode/TestKat.kt
@@ -0,0 +1,14 @@
+package testcode
+
+object TestKat {
+    @JvmStatic fun main(args: Array<String>) {
+        println("hey testing")
+        println("hey testing")
+        println("hey testing")
+        println("hey testing")
+        println("hey testing")
+        println("hey testing")
+        println("hey testing")
+        println("hey testing")
+    }
+}
